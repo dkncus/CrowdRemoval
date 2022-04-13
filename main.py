adbb7b612b778ae6baf995867f96df95945f2bae
David Kubala <dkubala@Davids-MacBook-Pro-2.local>
2022-04-13 16:53:37 +0100

    Sample Images

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Renderer import Renderer
 import os
+import cv2 as cv
 
 if __name__ == '__main__':
     # Root directory of the Large Mask Inpainting algorithm & video path to render
@@ -12,6 +13,11 @@ if __name__ == '__main__':
 
     # Create a renderer object and process video
     renderer = Renderer(lama_root)
-    renderer.render_video(video_path, starting_frame=144, show_frames=True, save_frames=True, bilateral_solve=False)   # Segment and Inpaint each frame individually
+    renderer.render_video(video_path, starting_frame=168, show_frames=True, save_frames=True, bilateral_solve=False)   # Segment and Inpaint each frame individually
     renderer.median_postprocess(video_path, num_steps_into_past=5)                                                     # Perform Median Postprocessing step
     # renderer.compile_video()
+
+    # Single image processing
+    # frame = cv.imread('input.png')
+    # image = renderer.render_frame(frame, 473, 0, seg_type = 'linear')
+    # cv.imwrite('output.png', image)
